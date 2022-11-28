1e0e174c3a95b0ad6a92b6a51ff637be61021999
estebaan444 <esteban.arquillo@institutvidreres.cat>
2022-11-28 09:20:05 +0100

    SplashScreeen and loginRegister

---
+++ app/src/main/java/com/estebi/coffeapp/SingInActivity.kt
@@ -0,0 +1,11 @@
+package com.estebi.coffeapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SingInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_singin)
+    }
+}
